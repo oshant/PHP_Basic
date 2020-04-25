bdfe659aee92c51d291d6881c88bdecb21deac0e
Oshant <roger.bosch.gasco@gmail.com>
2020-04-26 00:05:34 +0200

    Added conditionals.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <ul>
-    <li><a href="1-HelloWorld/HelloWorld.php"> Hola mundo</a> </li>
+    <li><a href="1-HelloWorld/HelloWorld.php"> Hello world </a></li>
+    <li><a href="2-Structure/structure.php"> Basic structure </a></li>
+    <li><a href="3-Variables/variables.php"> Basic variables </a></li>
+    <li><a href="3-Variables/constants.php"> Constants </a></li>
+    <li><a href="4-Conditionals/conditionals.php"> Conditionals </a></li>
 </ul>
