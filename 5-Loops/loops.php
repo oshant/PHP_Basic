b0c768903dad097c9d1d4550fa5faba34f1d5ec5
Oshant <roger.bosch.gasco@gmail.com>
2020-04-26 00:40:02 +0200

    Added loops.php

---
+++ 5-Loops/loops.php
@@ -0,0 +1,20 @@
+<?php
+
+/*  While is a common command in all programing languages, code loop when condition is true */
+
+$number = 0;
+while ($number <= 100) {
+    echo "<label> Element number $number </label> <br>";
+    //is necessary increment manually or the integer loop will not finish never
+    $number++;
+    if ($number == 99) {
+        // You can use break to exit loop
+        break;
+    }
+}
+
+//For is similar to while but include the autoincrement in definition
+$number = 0;
+for ($i = 0; $i < 100; $i++) {
+    echo "<label> Element number $i </label> <br>";
+}
--- index.php
+++ index.php
@@ -4,4 +4,5 @@
     <li><a href="3-Variables/variables.php"> Basic variables </a></li>
     <li><a href="3-Variables/constants.php"> Constants </a></li>
     <li><a href="4-Conditionals/conditionals.php"> Conditionals </a></li>
+    <li><a href="5-Loops/loops.php"> Loop </a></li>
 </ul>
