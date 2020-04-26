0b5e0d30f078cb4895381f60d6abc7ef490f057f
Oshant <roger.bosch.gasco@gmail.com>
2020-04-26 11:42:10 +0200

    Added _statements.php and functions.php

--- index.php
+++ index.php
@@ -5,4 +5,5 @@
     <li><a href="3-Variables/constants.php"> Constants </a></li>
     <li><a href="4-Conditionals/conditionals.php"> Conditionals </a></li>
     <li><a href="5-Loops/loops.php"> Loop </a></li>
+    <li><a href="7-Functions/functions.php"> Functions </a></li>
 </ul>
